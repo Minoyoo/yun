1c92409ed0ecce8a65f8e98d0dfc8f9b5d7f6200
Minoyoo <756345195@qq.com>
2021-05-27 05:02:50 +0800

    v0.0.4

--- create.sh
+++ create.sh
@@ -1,7 +1,6 @@
 #!/bin/expect
 spawn  bee start   --verbosity 5   --swap-endpoint http://172.104.45.51:8545  --debug-api-enable --clef-signer-enable  --clef-signer-endpoint /var/lib/bee-clef/clef.ipc --config /root/yun/bee.yaml
-expect "Password:"
-send "hu123456\n"
-expect "Confirm password:"
-send "hu123456\n"
-interact
+expect {
+    "Password:" {send "hu123456\n";exp_continue}
+    "Confirm password:" {send "hu123456\n";exp_continue}
+}
