f71c66ce78e4c644b88d6b5d939a9a5e3576b56c
Minoyoo <756345195@qq.com>
2021-05-27 04:54:11 +0800

    v0.0.3

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-spawn bee start   --verbosity 5   --swap-endpoint http://172.104.45.51:8545  --debug-api-enable --clef-signer-enable  --clef-signer-endpoint /var/lib/bee-clef/clef.ipc --config /root/donot/bee.yaml
+spawn bee start   --verbosity 5   --swap-endpoint http://172.104.45.51:8545  --debug-api-enable --clef-signer-enable  --clef-signer-endpoint /var/lib/bee-clef/clef.ipc --config /root/yun/bee.yaml
 expect "Password:"
 send "hu123456\n"
 interact
